2d9328d5e066748680df765e12e9bf02c26caad4
Ogenrwot Aaron <ogenrwotaron@gmail.com>
2019-10-18 10:19:17 +0300

    gitnore added build folder in client

---
+++ client/build/precache-manifest.208570eae996490d78bfd81228d52b00.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b932a54f317ca859df531ffdf76aa419",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8c86f80eef596d6a96c",
+    "url": "/static/js/2.b297e378.chunk.js"
+  },
+  {
+    "revision": "7ca51cd6d1dc91fea690",
+    "url": "/static/js/main.fdbe9b11.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
